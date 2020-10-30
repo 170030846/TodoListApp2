8ccfe9b9b98d698e20e0a3f0944dd2b5472f715e
170030846 <abhinavmuthavarapu@gmail.com>
2020-10-30 20:21:18 +0530

    todolist

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app" class="container">
+    <div class="todolist">
+ <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <h3 style="text-align: center">Todo APP</h3>
+   <TodoList/>
+    </div>
+   
+
+  </div>
+</template>
+
+<script>
+
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+
+*{
+  box-sizing: border-box;
+  
+}
+html{
+  background:#90f1c6;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 100%;
+}
+
+
+.container{
+  max-width: 600px;
+  margin: 0 auto;
+  
+}
+.todolist{
+   min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 1em;
+  background: #fff;
+  overflow: hidden;
+  box-shadow: 0 0 5px rgba(25,25,25,.25);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+
+.logo{
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,202 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+    <TodoItem v-for="todo in todosFiltered" :key="todo.id" :todo="todo" :checkAll="!anyRemaining" @removedTodo="removeTodo" @finishedEdit="finishedEdit">
+    </TodoItem>
+    </transition-group>
+
+    <div class="extra-container">
+      <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check All</label></div>
+      <div>{{ remaining }} items left</div>
+    </div>
+
+    <div class="extra-container">
+      <div>
+        <button :class="{ active: filter == 'all' }" @click="filter = 'all'">All</button>
+        <button :class="{ active: filter == 'active' }" @click="filter = 'active'">Active</button>
+        <button :class="{ active: filter == 'completed' }" @click="filter = 'completed'">Completed</button>
+      </div>
+
+      <div>
+        <transition name="fade">
+        <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+        </transition>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+// import axios from "axios";
+export default {
+  name: 'TodoList',
+  components: {
+    TodoItem,
+  },
+  data () {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+      filter: 'all',
+      todos: [
+        {
+          'id': 1,
+          'title': 'Create a TodoList App',
+          'completed': false,
+          'editing': false,
+        },
+        {
+          'id': 2,
+          'title': 'Create a TodoItem Component',
+          'completed': false,
+          'editing': false,
+        },
+      ]
+    }
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter(todo => !todo.completed).length
+    },
+    anyRemaining() {
+      return this.remaining != 0
+    },
+    todosFiltered() {
+      if (this.filter == 'all') {
+        return this.todos
+      } else if (this.filter == 'active') {
+        return this.todos.filter(todo => !todo.completed)
+      } else if (this.filter == 'completed') {
+        return this.todos.filter(todo => todo.completed)
+      }
+      return this.todos
+    },
+    showClearCompletedButton() {
+      return this.todos.filter(todo => todo.completed).length > 0
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      })
+      // const { title, completed } = newTodo;
+
+      // axios.post('https://jsonplaceholder.typicode.com/todos',{ title, completed })
+      // .then(res => this.todos = [...this.todos, res.data])
+      // .catch(err=>console.log(err));
+      this.newTodo = ''
+      this.idForTodo++
+    },
+    removeTodo(id) {
+      const index = this.todos.findIndex((item) => item.id == id)
+      this.todos.splice(index, 1)
+    },
+    checkAllTodos() {
+      this.todos.forEach((todo) => todo.completed = event.target.checked)
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed)
+    },
+    finishedEdit(data) {
+      const index = this.todos.findIndex((item) => item.id == data.id)
+      this.todos.splice(index, 1, data)
+    },
+    // created() {
+    //   axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    //     .then(res => this.todos = res.data)
+    //     .catch(err=> console.log(err))
+    // }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
+    }
+  }
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    animation-duration: 0.3s;
+  }
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: black;
+    }
+  }
+  .todo-item-left { // later
+    display: flex;
+    align-items: center;
+  }
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+    border: none;
+  }
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    &:focus {
+      outline: none;
+    }
+  }
+  .completed {
+    text-decoration: line-through;
+    color: grey;
+  }
+  .extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+    &:hover {
+      background: lightgreen;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  .active {
+    background: lightgreen;
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
